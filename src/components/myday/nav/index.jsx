865659a3899d8cb10e92c8b03983ec92216f2a25
akbarjon2000 <1543431a@gmail.com>
2022-01-01 23:55:39 +0900

    new component

--- src/components/myday/nav/index.jsx
+++ src/components/myday/nav/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { Container } from './style'
+import { ReactComponent as Dots } from "../../../assets/icons/three-dots.svg";
 
 const Navbar = () => {
     return (
         <Container>
-
+            <div className='header'>
+                <div className='display__row'>
+                    <p className='listTitle'>My Day</p>
+                    <Dots className='icon' />
+                </div>
+            </div>
         </Container>
     )
 }
--- src/components/myday/nav/style.js
+++ src/components/myday/nav/style.js
@@ -3,6 +3,11 @@ import styled from 'styled-components';
 export const Container = styled.div`
 width:100%;
 height:100px;
-background-color:yellow;
-
+.header{
+width:100%;
+height:100px;
+display:flex;
+padding:12px 16px 0;
+justify-content:space-between;
+}
 `
